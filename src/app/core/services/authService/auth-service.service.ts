24916ceb0a9ae2cae826074883264490b6d611e5
CarthyDurando <mcrabeto@gmail.com>
2023-11-15 00:04:26 +0400

    ADD ERROR HANDLER

--- src/app/core/services/authService/auth-service.service.ts
+++ src/app/core/services/authService/auth-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { User } from '../../models/user.interface';
 import { Album } from '../../models/album.model';
 
@@ -12,10 +12,21 @@ export class AuthServiceService {
   constructor(private http: HttpClient) { }
 
   runFirstRequest(lastNameLength: number) : Observable<any>{
-    return this.http.get(`https://jsonplaceholder.typicode.com/photos/${lastNameLength}`);
+    return this.http.get(`https://jsonplaceholder.typicode.com/photos/${lastNameLength}`).pipe(
+      catchError((error: any) => {
+        console.error('Error occurred in runFirstRequest:', error);
+        // Handle error as needed, such as displaying an error message to the user
+        return of({}); // Return an empty Observable to prevent further propagation of the error
+      })
+    );;
   }
 
   runSecondRequest(user: User) : Observable<any>{
-    return this.http.post(`https://jsonplaceholder.typicode.com/users`, user);
+    return this.http.post(`https://jsonplaceholder.typicode.com/users`, user).pipe(
+      catchError((error: any) => {
+        console.error('Error occurred in runSecondRequest:', error);
+        return of({}); 
+      })
+    );;
   }
 }
